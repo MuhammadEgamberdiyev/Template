006c9aca902591714cc1f275b6830bec22653763
MuhammadEgamberdiyev <muhammadegamberdiyev504@gmail.com>
2022-11-29 10:49:26 +0500

    demo

--- js/script.js
+++ js/script.js
@@ -14,3 +14,34 @@ btn.onmousemove = function(e){
     btn.style.setProperty('--x', x + 'px');
     btn.style.setProperty('--y', y + 'px');
 }
+
+
+
+
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+  });
+  
+  document.onkeydown = function (e) {
+    if (event.keyCode = 123) {
+      return false;
+    }
+  
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+      return false;
+    }
+    
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+      return false;
+    }
+    
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+      return false;
+    }
+    
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "U".charCodeAt(0)) {
+      return false;
+    }
+  
+  }
